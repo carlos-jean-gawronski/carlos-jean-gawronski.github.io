97f53679a8db60f3dfa562bc0442d1a66d133efb
Carlos Jean Gawronski <cereby222plays@gmail.com>
2020-05-20 11:36:52 -0300

    Updates

---
+++ precache-manifest.16e090f79220729a159c5aeb1952c814.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b27233d08d04bc977499e7314321b787",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6790409d564ef439b95",
+    "url": "/static/css/main.962877b8.chunk.css"
+  },
+  {
+    "revision": "f94c502f2fc960c34682",
+    "url": "/static/js/2.86cb5927.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.86cb5927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6790409d564ef439b95",
+    "url": "/static/js/main.45da506d.chunk.js"
+  },
+  {
+    "revision": "9c135115133c442c1418",
+    "url": "/static/js/runtime-main.1aeff1d9.js"
+  }
+]);
